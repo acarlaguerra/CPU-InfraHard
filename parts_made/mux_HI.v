02b0b2511c3eb7544970f8f9f42b52aa1ba2c18a
acarlaguerra <acarlaguerra@gmail.com>
2022-04-01 23:54:30 -0300

    Added Low funct MUX

---
+++ parts_made/mux_HI.v
@@ -0,0 +1,11 @@
+// MUX to select which comes from MULT and which from DIV
+module mux_HI (
+    input   wire    [1:0]   HISrc,
+    input   wire    [31:0]  Data_0,     // comes from MULT
+    input   wire    [31:0]  Data_1,     // comes from DIV
+    output  wire    [31:0]  Data_out
+);
+
+assign  Data_out    =   (HISrc[0])? Data_1  :   Data_0;
+
+endmodule
