3eee1cab35f2ca24f1eee548e2c930f5739463c3
acarlaguerra <acarlaguerra@gmail.com>
2022-04-02 00:39:09 -0300

    Added MUX Source from Shift reg

---
+++ parts_made/mux_Src.v
@@ -0,0 +1,11 @@
+//  MUX to select which comes from register A and which from register B
+module mux_Src(
+    input   wire    [1:0]   SHIFTSrc,
+    input   wire    [31:0]  Data_0,     // rt (REG B)
+    input   wire    [31:0]  Data_1,     // rs (REG A)
+    input   wire    [31:0]  Data_out
+);
+
+assign  Data_out    =   (SHIFTSrc[0])? Data_1  :   Data_0;
+
+endmodule
