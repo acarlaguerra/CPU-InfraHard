ac0b08a763d9d8e088c4b0c2739f794a7b5776e7
paulohltc <phltc@cin.ufpe.br>
2022-04-05 21:58:44 -0300

    atumalaca

--- parts_made/cpu.v
+++ parts_made/cpu.v
@@ -2,8 +2,18 @@ module cpu (
     input wire clk,
     input wire reset
 );
+// flags
+    wire overflow;
+    wire EQ;
+    wire GT;
+    wire zero;
+    wire NG; 
 
-// mux_ctrl
+// one purpose flag
+    wire LT; // slt
+
+
+// mux_ctrl selectors
     wire [2:0] IorD; 
     wire [1:0] EXCPCtrl;
     wire [1:0] RegDst;
@@ -18,4 +28,22 @@ module cpu (
     wire HISrc;
     wire [1:0] PCSrc;
 
+// write wires
+    wire PCWrite;
+    wire PCWriteCond; // ?
+    wire MemWrite;
+    wire IRWrite;
+    wire MDRWrite;
+    wire RegWrite;
+    wire ALUOutWrite;
+    wire EPCWrite;
+    wire HILOWrite;
+    wire RegAWrite;
+    wire RegBWrite; 
+
+ //  control wires
+    wire LSCtrl; // nao sei o tamanho ainda
+    wire SSCtrl; // nao sei o tamanho ainda
+
+    wire 
 endmodule          
