c8b4ac0dccf4e0675e61282b3465198927a22cc2
acarlaguerra <acarlaguerra@gmail.com>
2022-04-07 15:49:18 -0300

    Fixed div

--- parts_made/div.v
+++ parts_made/div.v
@@ -4,9 +4,10 @@ module div(
     input   wire            clk,
     input   wire            reset,
     input   wire            DivCtrl,
-    input   wire            Div0,
     input   wire    [31:0]  Data_A,
     input   wire    [31:0]  Data_B,
+
+    output  wire            Div0,
     output  reg     [31:0]  Data_HI,
     output  reg     [31:0]  Data_LO
 );
